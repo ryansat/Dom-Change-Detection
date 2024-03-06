0f958f746290f1f8d7ea05e961c631f43a213a19
Fajar Satria <fajarsat@hotmail.com>
2024-03-06 10:45:16 +0700

    init file

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <DomDetection/>
+  </div>
+</template>
+
+<script>
+import DomDetection from './components/DomDetection.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DomDetection
+  }
+}
+</script>
+
+<style>
+/* Add your styles here */
+</style>
